634e413bfa67a9d4639c92c2c8ec55364372f196
UdsonWillams <udson.willams@gmail.com>
2024-05-23 23:44:58 -0300

    formatter

--- - Estrutura-de-Dados-universidade/1 avaliação/00.py
+++ - Estrutura-de-Dados-universidade/1 avaliação/00.py
@@ -1,4 +1,4 @@
-def numVelasAniverasario(velasAlturaAniver):
+def numVelasAniverasario(velasAlturaAniver: int):
     maiorAltura = max(velasAlturaAniver)
     maior = velasAlturaAniver.count(maiorAltura)
     print(f"Número de velas com a altura maxima é de: {maior}")
@@ -7,13 +7,17 @@ def numVelasAniverasario(velasAlturaAniver):
 def main():
     velasAlturamain = []
     tamanhoVelasAltura = int(input("Qual o tamanho das velas?"))
-    while tamanhoVelasAltura <= 1 or tamanhoVelasAltura >= 10 ** 5:
-        tamanhoVelasAltura = int(input("VALOR INVALIDO\nPOR FAVOR DIGITE UM NOVO VALOR\nQual o tamanho "
-                                       "das velas?"))
+    while tamanhoVelasAltura <= 1 or tamanhoVelasAltura >= 10**5:
+        tamanhoVelasAltura = int(
+            input(
+                "VALOR INVALIDO\nPOR FAVOR DIGITE UM NOVO VALOR\nQual o tamanho "
+                "das velas?"
+            )
+        )
     altura = input("Qual a altura das velas?")
     for v in altura.split(" "):
         if v.isnumeric():
-            if 1 < int(v) < 10 ** 7:
+            if 1 < int(v) < 10**7:
                 velasAlturamain.append(int(v))
     while len(velasAlturamain) > tamanhoVelasAltura:
         velasAlturamain.pop()
